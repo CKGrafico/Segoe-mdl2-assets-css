d5b5b4a933f4cde890ce0c1a41057cb5a5b8a942
CKGrafico <quique@ckgrafico.com>
2015-09-11 14:35:38 +0200

    update styling

--- app.js
+++ app.js
@@ -6,8 +6,19 @@
 		for (var i = 0, l = dirtyList.length; i < l; i++) {
 			var icon = dirtyList[i].split('::before')[0].slice(1);
 			cleanList.push(icon);
-			$('.icons').append('<li><div class="' + icon + '"></div><span>' + icon + '</span></li>');
+			$('.icons').append('<li class="icons-item"><div class="icons-icon ' + icon + '"></div><span class="icons-tag">' + icon + '</span></li>');
 		}
 		
+		$('.icons-item').on('click mouseover', function() {
+			$('.icons-item').removeClass('icons-item--touched');
+			var $this = $(this);
+			var $tag = $this.find('.icons-tag');
+			var $pos = $this.position();
+			var $width = $this.width();
+			var $tagWidth = $tag.width();
+			var $height = $this.height();
+			$this.addClass('icons-item--touched')
+			$tag.css({top: $pos.top + $height, left: $pos.left + $with});
+		});
 	}
 })(jQuery);
